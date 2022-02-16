382937c6d17b65aa809c4d22507adb935778a5b4
Krisanu <kisanudey886@gmail.com>
2022-02-16 21:07:20 +0530

    MERN using

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>this is login or signup page using MERN stack</h1>
+      <h1>this is login or signup page using MERN</h1>
     </>
   );
 }
